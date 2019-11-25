ec7479d8bfb7005bd3fef493a0bc8a51fd8d9406
Sergio Panduro <panduro.sergio@outlook.es>
2019-11-25 13:03:34 -0600

    nuevos cambios

--- server/rutas/usuario.js
+++ server/rutas/usuario.js
@@ -1,13 +1,40 @@
 
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
 
 const app = express();
 const Usuario = require('../models/usuario')
 
-/* app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
-}); */
+app.get('/usuario', function(req, res) {
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+
+    Usuario.find({})
+            .skip(desde)
+            .limit(limite)
+            .exec((err, usuarios) => {
+                
+                if(err) {
+                    return res.status(400).json({
+                        ok:false,
+                        err
+                    });
+                }
+
+                res.json({
+                    ok:true,
+                    usuarios
+                })
+
+            })
+});
 
 app.post('/usuario', function(req, res) {
 
@@ -40,26 +67,22 @@ app.post('/usuario', function(req, res) {
 app.put('/usuario/:id', function(req, res) {
 
     let id = req.params.id;
-    let body = req.body;
-    console.log(id);
-    console.log(body);
-    Usuario.findOneAndUpdate(id, {$set : body } , { new: true }, (err, usuarioDB) => {
-        console.log(usuarioDB);
+    let body = _.pick( req.body, ['nombre', 'email', 'img', 'role', 'estado']  );
+
+    Usuario.findOneAndUpdate(id,  body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
         if(err){
             return res.status(400).json({
                 ok: false,
                 err
             });
-            console.log('error');
         }
-        else{
-            console.log('todo bien');
+    
             res.json({
                 ok:true,
                 usuario: usuarioDB,
             });
-        }
+        
     })
 
 });
