d527e1f8115986d62cb9baeecd79f82a9a888557
Sergio Panduro <panduro.sergio@outlook.es>
2019-12-06 10:52:03 -0600

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://honeypot:sergio21@cluster0-dstw1.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
