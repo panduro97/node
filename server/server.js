55690662603579e556c0b2038ad16ddff1ad6b3d
Sergio Panduro <panduro.sergio@outlook.es>
2019-12-06 14:34:34 -0600

    cambios

--- server/server.js
+++ server/server.js
@@ -9,10 +9,13 @@ const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
+
 app.use(bodyParser.json())
-app.use( require('./rutas/usuario.js'))
+
+// Configuracion de rutas
+app.use( require('./rutas/index.js'))
+
 
 
 
